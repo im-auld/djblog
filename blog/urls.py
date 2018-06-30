aec14a868054698516c6b45190939761404292bc
Ian AUld <imauld@gmail.com>
2018-06-30 14:19:11 -0700

    Did a blog thing

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,11 @@
 from django.conf.urls import url
 from django.contrib import admin
 
+from . import views
+
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
+    url(r"^$", views.BlogListView.as_view(), name="blog_list_view"),
+    url(r'^post/(?P<post_id>\d+)/$', views.BlogDetailView.as_view(), name='post_detail_view'),
+    url(r'^post/new/$', views.NewPostView.as_view(), name='new_post_view'),
+    url(r'^post/edit/(?P<post_id>\d+)/$', views.EditPost.as_view(), name='edit_post_view'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,55 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.http.request import HttpRequest
+from django.utils import timezone
+from django.views import View
+
+from .models import Post
+from .forms import PostForm
+
 
 # Create your views here.
+class BlogListView(View):
+    def get(self, request):
+        posts = Post.objects.filter(
+            published_date__lte=timezone.now()
+        ).order_by('published_date')
+        return render(request, "blog/post_list.html", {"posts": posts})
+
+
+class BlogDetailView(View):
+    def get(self, request, post_id):
+        post = Post.objects.get(pk=post_id)
+        return render(request, "blog/post_detail.html", {"post": post})
+
+
+class NewPostView(View):
+    def get(self, request):
+        form = PostForm()
+        return render(request, "blog/post_edit.html", {"form": form})
+
+    def post(self, request: HttpRequest):
+        form = PostForm(data=request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('post_detail_view', post_id=post.pk)
+        return render(request, 'blog/post_edit.html', {'form': form})
+
+
+class EditPost(View):
+    def get(self, request, post_id):
+        post = Post.objects.get(pk=post_id)
+        form = PostForm(instance=post)
+        return render(request, "blog/post_edit.html", {"form": form})
+
+    def post(self, request, post_id):
+        post = Post.objects.get(pk=post_id)
+        form = PostForm(data=request.POST, instance=post)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.save()
+            return redirect('post_detail_view', post_id=post.pk)
+        return render(request, 'blog/post_edit.html', {'form': form})
